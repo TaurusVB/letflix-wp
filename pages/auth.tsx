2a40c222a81dc8110112fc9c72f135e8f3f01735
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-11-01 20:29:34 +0200

    fix with fonts in nextjs

---
+++ pages/auth.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Auth = () => {
+  return (
+    <div className="relative h-full w-full bg-[url('./images/hero.jpg')]"></div>
+  );
+};
+
+export default Auth;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="">Netflix clone</h1>;
+  return <h1 className="text-2xl text-green-500">Netflix clone</h1>;
 }
