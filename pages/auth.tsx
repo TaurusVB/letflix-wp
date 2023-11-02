9801aac819d3e0e1678ea1b82852a825478320c7
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-11-02 19:25:28 +0200

    created prisma models for app

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -19,7 +19,7 @@ const Auth = () => {
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className=" bg-black w-full h-full lg:bg-opacity-50">
         <nav className=" px-12 py-5">
-          <Image height={12} width={100} src="/images/logo.png" alt="Logo" />
+          <Image height={12} width={160} src="/images/logo.png" alt="Logo" />
           <div className="flex justify-center">
             <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
               <h2 className="text-white text-4xl mb-8 font-semibold">
