8c0664f9ce3193868a8d6a21ea6136daa7953b4b
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-11-04 18:17:45 +0200

    finish doing Github and Google authentication

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,23 @@
-import NextAuth from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
 import { compare } from "bcrypt";
+
 import prismadb from "@/libs/prismadb";
 
-export default NextAuth({
+export const authOptions: AuthOptions = {
   providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
     Credentials({
       id: "credentials",
       name: "Credentials",
@@ -50,9 +63,12 @@ export default NextAuth({
     signIn: "/auth",
   },
   debug: process.env.NODE_ENV === "development",
+  adapter: PrismaAdapter(prismadb),
   session: { strategy: "jwt" },
   jwt: {
     secret: process.env.NEXTAUTH_JWT_SECRET,
   },
   secret: process.env.NEXTAUTH_SECRET,
-});
+};
+
+export default NextAuth(authOptions);
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,12 +1,15 @@
 import { useCallback, useState } from "react";
 
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { signIn } from "next-auth/react";
 
 import axios from "axios";
 
 import Input from "@/components/Input";
-import { useRouter } from "next/router";
+
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 const Auth = () => {
   const router = useRouter();
@@ -98,6 +101,20 @@ const Auth = () => {
               >
                 {variant === "login" ? "Login" : "Sign up"}
               </button>
+              <div className=" flex flex-row it gap-4 mt-8 justify-center">
+                <div
+                  onClick={() => signIn("google", { callbackUrl: "/" })}
+                  className=" w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+                >
+                  <FcGoogle size={30} />
+                </div>
+                <div
+                  onClick={() => signIn("github", { callbackUrl: "/" })}
+                  className=" w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+                >
+                  <FaGithub size={30} />
+                </div>
+              </div>
               <p className=" text-neutral-500 mt-12">
                 {variant === "login"
                   ? "First time using Netflix?"
