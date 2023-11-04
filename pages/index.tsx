e63f0f14630448878f26858239f5ef7fa799b642
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-11-04 19:00:30 +0200

    created currentUser route, protecting routes functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,34 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
-  return <h1 className="text-2xl text-green-500">Netflix clone</h1>;
+  const { data: user } = useCurrentUser();
+
+  return (
+    <>
+      <h1 className="text-2xl text-green-500">Netflix clone</h1>
+      <p className=" text-white">Logged in as: {user?.email}</p>
+      <button onClick={() => signOut()} className=" h-12 w-full bg-yellow-400">
+        LogOut!
+      </button>
+    </>
+  );
 }
