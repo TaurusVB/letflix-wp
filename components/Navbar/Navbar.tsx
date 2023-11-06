d5b5847a9f49d53b839185d1c5c32689eaa7db1e
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-11-06 18:27:24 +0200

    created AccountMenu component, improvement Navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,12 +1,30 @@
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { BsBell, BsChevronDown, BsSearch } from "react-icons/bs";
 
 import NavbarItem from "../NavbarItem";
-
-import { BsBell, BsChevronDown, BsSearch } from "react-icons/bs";
 import MobileMenu from "../MobileMenu";
+import AccountMenu from "../AccountMenu";
+
+const TOP_OFFSET = 66;
 
 const Navbar = () => {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [showAccountMenu, setShowAccountMenu] = useState(false);
+  const [showBackground, setShowBackground] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= TOP_OFFSET) {
+        setShowBackground(true);
+      } else {
+        setShowBackground(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   const items = useMemo(
     () => [
@@ -24,9 +42,17 @@ const Navbar = () => {
     setShowMobileMenu((current) => !current);
   }, []);
 
+  const toggleAccountMenu = useCallback(() => {
+    setShowAccountMenu((current) => !current);
+  }, []);
+
   return (
     <nav className=" w-full fixed z-40">
-      <div className=" px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900/90">
+      <div
+        className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${
+          showBackground ? "bg-zinc-900/90" : ""
+        }`}
+      >
         <img src="/images/logo.png" alt="logo" className="h-4 lg:h-7 " />
         <div className=" flex-row ml-8 gap-7 hidden lg:flex">
           {items.map((label) => (
@@ -38,7 +64,11 @@ const Navbar = () => {
           className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative"
         >
           <p className="text-white text-sm">Browse</p>
-          <BsChevronDown className=" text-white transition " />
+          <BsChevronDown
+            className={`text-white transition ${
+              showMobileMenu ? "rotate-180" : "rotate-0"
+            }`}
+          />
           <MobileMenu visible={showMobileMenu} items={items} />
         </div>
         <div className="flex flex-row ml-auto gap-7 items-center">
@@ -48,10 +78,19 @@ const Navbar = () => {
           <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
             <BsBell />
           </div>
-          <div className=" flex flex-row items gap-2 cursor-pointer relative">
+          <div
+            onClick={toggleAccountMenu}
+            className=" flex flex-row items-center gap-2 cursor-pointer relative"
+          >
             <div className=" w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
               <img src="/images/default-green.png" alt="avatar" />
             </div>
+            <BsChevronDown
+              className={`text-white transition ${
+                showAccountMenu ? "rotate-180" : "rotate-0"
+              }`}
+            />
+            <AccountMenu visible={showAccountMenu} />
           </div>
         </div>
       </div>
