a7be8044e9d872469268242b7f3c75f36ede7eac
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-11-06 21:53:55 +0200

    created Billboard component, route for get random movie for billboard, refresh Prisma models

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { getSession } from "next-auth/react";
 
 import Navbar from "@/components/Navbar";
 import useCurrentUser from "@/hooks/useCurrentUser";
+import Billboard from "@/components/Billboard";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -25,6 +26,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
+      <Billboard />
     </>
   );
 }
