6a1047e91243ec661aebda5d74d72801664074fd
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-11-16 16:56:19 +0200

    fix bug with auth page redirecting

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -10,10 +10,10 @@ import useCurrentUser from "@/hooks/useCurrentUser";
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
 
-  if (!session) {
+  if (session) {
     return {
       redirect: {
-        destination: "/auth",
+        destination: "/",
         permanent: false,
       },
     };
